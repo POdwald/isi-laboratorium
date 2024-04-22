b750821c4d96f560bee1370530f833f73d8a992e
POdwald <141851599+POdwald@users.noreply.github.com>
2024-04-22 04:02:40 +0200

    Added login, logout, password reset/change functionalities

--- lab5/blog/forms.py
+++ lab5/blog/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from .models import Post
 
@@ -7,3 +9,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
--- lab5/mysite/urls.py
+++ lab5/mysite/urls.py
@@ -1,7 +1,17 @@
-from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+
+from django.contrib.auth import views
+from blog.views import SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('accounts/signup/', SignUpView.as_view(), name='signup'),
+    path('accounts/password_change/', views.PasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password_reset/done', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('accounts/password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('', include('blog.urls')),
 ]
